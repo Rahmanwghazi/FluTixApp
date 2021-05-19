2ad3467a6938efada9b2cac58e57e008230f2c75
rahmanwghazi <rahmanwghazi@student.telkomuniversity.ac.id>
2021-05-19 23:59:11 +0700

    main page nav

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -6,6 +6,17 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int bottomNavBarIndex;
+  PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    bottomNavBarIndex = 0;
+    pageController = PageController(initialPage: bottomNavBarIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,18 +29,72 @@ class _MainPageState extends State<MainPage> {
             child: Container(
           color: Color(0xFFF6F7F9),
         )),
-        ListView(),
+        PageView(
+          controller: pageController,
+          onPageChanged: (index) {
+            setState(() {
+              bottomNavBarIndex = index;
+            });
+          },
+          children: [
+            Center(
+              child: Text("New Movie"),
+            ),
+            Center(
+              child: Text("My Tickets"),
+            ),
+          ],
+        ),
         Align(
           alignment: Alignment.bottomCenter,
           child: ClipPath(
             clipper: BottomNavBarClipper(),
             child: Container(
-                height: 70,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20)))),
+              height: 70,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+              child: BottomNavigationBar(
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                selectedItemColor: mainColor,
+                unselectedItemColor: Color(0xFFE5E5E5),
+                currentIndex: bottomNavBarIndex,
+                onTap: (index) {
+                  setState(() {
+                    bottomNavBarIndex = index;
+                    pageController.jumpToPage(index);
+                  });
+                },
+                items: [
+                  BottomNavigationBarItem(
+                    label: "New Movie",
+                    //style: GoogleFonts.raleway(
+                    //  fontSize: 13, fontWeight: FontWeight.w600)),
+                    icon: Container(
+                      margin: EdgeInsets.only(bottom: 6),
+                      height: 20,
+                      child: Image.asset((bottomNavBarIndex == 0)
+                          ? "assets/ic_movies.png"
+                          : "assets/ic_movies_grey.png"),
+                    ),
+                  ),
+                  BottomNavigationBarItem(
+                      label: "My Tickets",
+                      //style: GoogleFonts.raleway(
+                      //  fontSize: 13, fontWeight: FontWeight.w600)),
+                      icon: Container(
+                        margin: EdgeInsets.only(bottom: 6),
+                        height: 20,
+                        child: Image.asset((bottomNavBarIndex == 1)
+                            ? "assets/ic_tickets.png"
+                            : "assets/ic_tickets_grey.png"),
+                      )),
+                ],
+              ),
+            ),
           ),
         ),
         Align(
@@ -39,6 +104,7 @@ class _MainPageState extends State<MainPage> {
                 width: 46,
                 margin: EdgeInsets.only(bottom: 42),
                 child: FloatingActionButton(
+                  onPressed: () {},
                   elevation: 0,
                   backgroundColor: accentColor2,
                   child: SizedBox(
