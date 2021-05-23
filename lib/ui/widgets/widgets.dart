db816fda4ee8ff25e58e660ee06b43ab93e7ef0a
rahmanwghazi <rahmanwghazi@student.telkomuniversity.ac.id>
2021-05-23 20:30:50 +0700

    movie card + rating

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,4 +1,8 @@
 import 'package:flutix/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:flutix/models/models.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 part 'selectable_box.dart';
+part 'movie_card.dart';
+part 'rating_stars.dart';
